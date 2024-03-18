deb24a5d3fb8d6855f629e865b61c807f4c3d64f
kirillmc <kirillmcdev@gmail.com>
2024-03-18 12:24:54 +0300

    added layers, di container & upgraded db-client

---
+++ internal/api/chat/create.go
@@ -0,0 +1,18 @@
+package chat
+
+import (
+	"context"
+
+	"github.com/kirillmc/chat-server/internal/converter"
+	desc "github.com/kirillmc/chat-server/pkg/chat_v1"
+)
+
+func (i *Implementation) CreateChat(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	id, err := i.chatService.CreateChat(ctx, converter.ToChatFromCreateRequest(req))
+	if err != nil {
+		return nil, err
+	}
+	return &desc.CreateResponse{
+		Id: id,
+	}, nil
+}
